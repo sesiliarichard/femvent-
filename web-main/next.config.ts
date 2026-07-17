b5a30c538f169a31d78cb3ec1894f85b9d05bf21
sesiliarichard <sesiliarichard2003@gmail.com>
2026-07-17 11:51:04 +0300

    Registration flow, email, and web-main Supabase connection

--- web-main/next.config.ts
+++ web-main/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone',
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
