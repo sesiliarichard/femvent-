00daec61291c3353504ea197249b7d04c0c78415
sesiliarichard <sesiliarichard2003@gmail.com>
2026-08-27 09:49:39 +0300

    ...

--- web-main/src/components/EditableImage.tsx
+++ web-main/src/components/EditableImage.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { supabase } from '../services/supabase';
+import { supabase } from '../lib/supabase';
 
 interface EditableImageProps {
   src: string;
