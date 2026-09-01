63140b1f39906e57e3aeb91d1310e2f56dd0d5b4
sesiliarichard <sesiliarichard2003@gmail.com>
2026-09-01 14:34:01 +0300

    ...

--- admin-main/next.config.js
+++ admin-main/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/admin',
+  assetPrefix: '/admin',
   images: {
     remotePatterns: [
       {
