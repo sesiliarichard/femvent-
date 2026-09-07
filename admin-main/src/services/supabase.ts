497173614a57148b48f9ec659b557f5b56ae7b44
sesiliarichard <sesiliarichard2003@gmail.com>
2026-09-07 12:16:40 +0300

    ...

--- admin-main/src/services/supabase.ts
+++ admin-main/src/services/supabase.ts
@@ -4,3 +4,6 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+if (typeof window !== 'undefined') {
+    (window as any).supabase = supabase;
+  }
